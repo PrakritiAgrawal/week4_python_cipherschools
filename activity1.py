3825c21468e585eb21d654fa184041ef8051a8bd
PrakritiAgrawal <119535105+PrakritiAgrawal@users.noreply.github.com>
2023-01-10 21:35:18 +0530

    Update activity1.py

--- activity1.py
+++ activity1.py
@@ -1,88 +1,95 @@
-File Access Modes
-Read only ('r')
-Read and Write ('r+')
-Write Only ('w')
-Write and Read ('w+')
-Append only ('a')
-Append and Read ('a+')
-1. Text Files
-Opening a| file
-file =open("random.txt","w")
-file.write("blah blah blah blah")
-19
-file.write("\n new line")
-10
-file.close()
-Writing to a File
-write
-writelines
-file = open("random.txt","w")
-file.write("Joey Tribbiani")
-14
-a=["Joey","Doesn't","Share","Food!!!"]
-file.writelines(a)
- file.close()
-Reading from a File
-read
-readline
-readlines
-file = open("sample.txt","r")
-file.read()
-'Joey\nPheobe\nChandler\nRoss\nMonica\nRachel\n'
-print(file.read())
-file.close()
-Streams
-Iterables which can be iterated in a single direction
-They dont have starting and ending point
-Moving the Cursor
-seek(n): takes the file handle to the nth byte from the beginning.
-Smart way of Opening files
-With the "with" statement, you get better syntax and execptions handling. "The with statement simplifies exception handling by encapsulation common preparation and cleanup tasks." In addition, it will automatically close the file. The way statement provides a way for ensuring that a clean-up is always used.
+# File Access Mode
+# Read Only => r
+# Read and Write => r+
+# Write Only => w 
+# Write and Read  => w+
+# Append Only => a
+# Append and Read => a+
 
-Context Programming
+"""TEXT FILES"""
+file=open("random.txt","w")
+file.write("hello hello hello")
+file.write("\n new line")
+file.close() #Closing a file is important because when you open a file in write mode you are blocking the operations from the processes. You have allocated a resource/blocked a resource so you should release that resource once you are done with it.
+
+'''Writing a file'''
+file=open("random.txt","w")
+file.write("Harshita Bajpai")
+a=["harshita","bajpai","sanchita","nivedita"]
+file.writelines(a)
+file.close()
+
+'''Reading a file'''
+# read
+# readline
+# readlines
+
+file=open("sample.txt","r")
+a=file.read()
+print(a)
+print(file.read()) #The cursor goes in the end, so didn't read the things before the cursor.
+
+
+
+"""Smarter Way Of Opening Files"""
+
+#Context Programming
+
 with open("random.txt","r") as file:
     print(file.read())
-Joey TribbianiJoeyDoesn'tShareFood!!!
+
+#print(file.read())
+
+#Using with ---> Once the context starts it opens the file and once the context ends it closes the file.
+
 class A:
     def __enter__(self):
         print("Entered")
         return 1
-    def __exit__(self, *args):
+    def __exit__(self,*args):
         print("Exitted")
         print(args)
         return True
+
 with A() as x:
     print(x)
-    print("inside Context")
-print("Outside Context")
-Entered
-1
-inside Context
-Exitted
-(None, None, None)
-Outside Context
-JSON Files
-JavaScript Object Notation
+    print("inside context")
+    raise Exception
 
-json.dump(obj,fileObj)
-json.dumps(obj)
-json.load(JSONfile)
-json.loads(JSONfile)
-JSON is like a dictionaries
+print("outside context")
 
-JSON Rules
-keys can only be strings and numbers
-values can only be array, json, strings, numbers, boolean, null
-a = {
-    "name":"joey",
-    "marks":100,
-    "language": [
-        "c++","python","go","rust"
-    ]
+#Whatever happens in a context remains in a context
+
+
+"""JSON FILES"""
+#Text representaion of data.
+#JSON RULE => In json your keys cn onl;y be strings and numbers and value can only be array,json,numbers,boolean,null.
+
+'''
+<html>
+    <body>
+        Hi
+    </body>
+</html>
+{
+    "html":{
+        "body":"Hi"
+    }
 }
+'''
+
+a={
+    "name":"harshita bajpai",
+    "marks":100,
+    "languages":[
+        "c++",
+        "python",
+        "go",
+        "rust"
+        ]
+}
+
 import json
-s = json.dumps(a)
-type(s)
-str
+s=json.dumps(a)
+type(a)
 print(s)
-{"name": "joey", "marks": 100, "language": ["c++", "python", "go", "rust"]}
